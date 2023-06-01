812d617728e7eeaab6162d2c949252e981966a87
mstol <shoshinmas@protonmail.com>
2023-06-01 22:46:15 +0200

    initial commit

---
+++ src/main/java/com/zaprogramujzycie/finanse/FinanseApplication.java
@@ -0,0 +1,13 @@
+package com.zaprogramujzycie.finanse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanseApplication.class, args);
+	}
+
+}
