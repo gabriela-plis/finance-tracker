4cea0cacc252510c0d3f480680cc4354411a4ea3
gabriela <gabriela.plis.webdev@gmail.com>
2023-07-03 11:37:47 +0200

    Defined swagger endpoints. Fixed typo in IncomeController.

--- backend/src/main/java/com/zaprogramujzycie/finanse/income/IncomeController.java
+++ backend/src/main/java/com/zaprogramujzycie/finanse/income/IncomeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users/{userId}/income")
+@RequestMapping("/users/{userId}/incomes")
 @RequiredArgsConstructor
 public class IncomeController {
 
