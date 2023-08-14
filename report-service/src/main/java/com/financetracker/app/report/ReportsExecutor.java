77a05339ea4022395e0c2a8737a0dd44f2067bfd
gabriela <gabriela.plis.webdev@gmail.com>
2023-08-14 15:36:27 +0200

    ReportsExecutor - added methods to generate reports and create mails.

--- report-service/src/main/java/com/financetracker/app/report/ReportsExecutor.java
+++ report-service/src/main/java/com/financetracker/app/report/ReportsExecutor.java
@@ -1,43 +1,70 @@
 package com.financetracker.app.report;
 
-import com.financetracker.app.rabbitmq.QueueSender;
+import com.financetracker.app.mail.MailDTO;
+import com.financetracker.app.mail.MailService;
+import com.financetracker.app.mail.Template;
 import com.financetracker.app.report.generators.MonthlyReportsGenerator;
 import com.financetracker.app.report.generators.ReportsGenerator;
 import com.financetracker.app.report.generators.WeeklyReportsGenerator;
 import com.financetracker.app.report.types.Report;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@Component
+@RequiredArgsConstructor
 public class ReportsExecutor {
-    Set<ReportsGenerator> generators;
-    QueueSender queueSender;
+
+    private final List<ReportsGenerator<?>> generators;
+    private final MailService mailService;
 
     @Scheduled(cron = "0 0 8 1 1/1 *")
     public void executeMonthlyReports() {
-        ReportsGenerator generator = getGenerator(MonthlyReportsGenerator.class);
-        List<Report> reports = generator.generate();
-        sendReports(reports);
+        List<ReportsGenerator<?>> generators = getGenerators(MonthlyReportsGenerator.class);
+
+        Map<Template, List<? extends Report>> reports = generateReports(generators);
+        List<MailDTO> mails = createMails(reports);
+        sendMails(mails);
     }
 
     @Scheduled(cron = "0 0 8 * * MON")
     public void executeWeeklyReports() {
-        ReportsGenerator generator = getGenerator(WeeklyReportsGenerator.class);
-        List<Report> reports = generator.generate();
-        sendReports(reports);
+        List<ReportsGenerator<?>> generators = getGenerators(WeeklyReportsGenerator.class);
+
+        Map<Template, List<? extends Report>> reports = generateReports(generators);
+        List<MailDTO> mails = createMails(reports);
+        sendMails(mails);
     }
 
-    private void sendReports(List<Report> reports) {
-        for(Report report : reports) {
-            queueSender.send(report);
-        }
+    private Map<Template, List<? extends Report>> generateReports(List<ReportsGenerator<?>> generators) {
+        return generators.stream()
+            .collect(Collectors.toMap(
+                ReportsGenerator::getTemplate,
+                ReportsGenerator::generate
+            ));
     }
 
-    private <T extends ReportsGenerator> T getGenerator(Class<T> clazz) {
-        return generators.stream().filter(clazz::isInstance)
-            .map(clazz::cast)  //TODO wonder why it needs to be casting to T
-            .findFirst()
-            .orElseThrow(IllegalStateException::new); //TODO what type of exception should be throw
+    private List<MailDTO> createMails(Map<Template, List<? extends Report>> reports) {
+        return reports.entrySet().stream()
+            .flatMap(entry -> entry.getValue().stream()
+                .map(report -> mailService.createMail(report, entry.getKey(), entry.getKey().getTitle()))
+            )
+            .toList();
+    }
+
+    private void sendMails(List<MailDTO> mails) {
+        mails.forEach(mailService::sendMail);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends ReportsGenerator<?>> List<ReportsGenerator<?>> getGenerators(Class<T> clazz) {
+        return (List<ReportsGenerator<?>>) generators.stream()
+            .filter(clazz::isInstance)
+            .map(clazz::cast)
+            .toList();
     }
 }
