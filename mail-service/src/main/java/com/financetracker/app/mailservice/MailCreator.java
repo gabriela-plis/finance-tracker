5fc56bba1b08083c6f293bef49c653174d7e20e4
gabriela <gabriela.plis.webdev@gmail.com>
2023-09-26 13:16:11 +0200

    Added MailCreator.

---
+++ mail-service/src/main/java/com/financetracker/app/mailservice/MailCreator.java
@@ -0,0 +1,50 @@
+package com.financetracker.app.mailservice;
+
+import com.financetracker.api.mail.MailDTO;
+import com.financetracker.api.mail.Template;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Component
+@RequiredArgsConstructor
+public class MailCreator {
+
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public MimeMessage createMessage(MailDTO mail) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(sender);
+            helper.setTo(mail.recipient());
+            helper.setSubject(mail.title());
+            helper.setText(getHtmlTemplate(mail), true);
+
+            return helper.getMimeMessage();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getHtmlTemplate(MailDTO mail) {
+        Context context = new Context();
+        context.setVariables(mail.templateProperties());
+        return templateEngine.process(extractHtmlTemplateName(mail.template()), context);
+    }
+
+    private String extractHtmlTemplateName(Template template) {
+        return template.toString().toLowerCase().replaceAll("_", "-") + "-template";
+    }
+}
--- mail-service/src/main/java/com/financetracker/app/mailservice/MailService.java
+++ mail-service/src/main/java/com/financetracker/app/mailservice/MailService.java
@@ -1,45 +1,20 @@
 package com.financetracker.app.mailservice;
 
 import com.financetracker.api.mail.MailDTO;
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
     private final JavaMailSender javaMailSender;
-    private final TemplateEngine templateEngine;
-
-    @Value("${spring.mail.username}")
-    private String sender;
+    private final MailCreator mailCreator;
 
     public void sendMail(MailDTO mail) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-            helper.setFrom(sender);
-            helper.setTo(mail.recipient());
-            helper.setSubject(mail.title());
-
-            //define thymeleaf html template -> resources/templates/
-            //create Context and setVariables(map <String, Object> properties)
-            Context context = new Context();
-            context.setVariables(mail.templateProperties());
-            //create String object: templateEngine.process("template-name", context)
-            String html = templateEngine.process("general-weekly-report-template", context);
-            //set it as helper text
-            helper.setText(html, true);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+       MimeMessage message = mailCreator.createMessage(mail);
+       javaMailSender.send(message);
     }
 }
